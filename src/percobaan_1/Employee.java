e13f1935db588b790e53373618f0590ae830b056
Yudas <yudasmalabi@gmail.com>
2021-11-10 13:23:39 +0700

    done percobaan 1

---
+++ src/percobaan_1/Employee.java
@@ -0,0 +1,9 @@
+package percobaan_1;
+
+public class Employee {
+    protected String name;
+
+    public String getEmployeeInfo() {
+        return "Name = " + name;
+    }
+}
---
+++ src/percobaan_1/Payable.java
@@ -0,0 +1,5 @@
+package percobaan_1;
+
+public interface Payable {
+    public int getPaymentAmount();
+}
