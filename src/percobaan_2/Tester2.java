81d8ed4df081ed059776d936c207d76cf8b14900
Yudas <yudasmalabi@gmail.com>
2021-11-10 13:25:20 +0700

    done percobaan 2

---
+++ src/percobaan_2/Tester2.java
@@ -0,0 +1,16 @@
+package percobaan_2;
+
+import percobaan_1.Employee;
+import percobaan_1.PermanentEmployee;
+
+public class Tester2 {
+
+    public static void main(String[] args) {
+        PermanentEmployee pEmp = new PermanentEmployee("Dedik", 500);
+        Employee e;
+        e = pEmp;
+        System.out.println("" + e.getEmployeeInfo());
+        System.out.println("----------------------");
+        System.out.println("" + pEmp.getEmployeeInfo());
+    }
+}
